10136e3293de70b184dea49d42ca27adb45206a2
Thibaud Gilles <thibaud.gilles@gmail.com>
2024-01-30 15:28:46 +0100

    premiere route GET

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,40 @@
 var express = require('express');
 var router = express.Router();
+require('../models/connection');
+const Trip = require('../models/trips');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/trips', (req, res) => {
+  Trip.find({ 
+    departure: {$regex: new RegExp(req.body.departure, 'i')}, 
+    arrival: {$regex: new RegExp(req.body.arrival, 'i')}
+  })
+  .then(data => {
+    if(data) {
+      res.json({ result: true, tripsFound: data})
+    } else {
+      res.json({ result: false, error: "No trip found."})
+    }
+  })
+})
 
+router.post('/book', (req, res) => {
 
+})
+
+router.put('/purchase', (req, res) => {
+
+})
+
+router.delete('/cancel', (req, res) => {
+
+})
+
+router.get('/display', (res, req) => {
+
+})
 module.exports = router;
