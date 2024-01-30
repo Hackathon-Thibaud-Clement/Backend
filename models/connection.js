5015365cb2ba2581eec2fad57450208011f2a9da
Clément Boulanger <clement.boulangerpro@gmail.com>
2024-01-30 15:34:44 +0100

    models first commit

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin:IUurD4jq3VJRAB9s@cluster0.sbev9wq.mongodb.net/'
+const connectionString = 'mongodb+srv://admin:IUurD4jq3VJRAB9s@cluster0.sbev9wq.mongodb.net/tickethack'
 
 mongoose.connect(connectionString, {connectTimeoutMS: 2000})
 .then(() => console.log('Database connected'))
 .catch (error => console.error(error));
+
+module.exports = connectionString;
