e9967f2ec0f6ee07e4db1fe3b519029b46264c57
Thibaud Gilles <thibaud.gilles@gmail.com>
2024-01-31 15:04:56 +0100

    cors ajouté dans app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const cors = require('cors');
+app.use(cors());
 var app = express();
 
 // view engine setup
